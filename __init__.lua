f8d9c8442ec9add317a45d602dc2df8b03076847
MF_HaZe <102245719+MFHaZe@users.noreply.github.com>
2024-09-02 23:30:43 +0100

    Update __init__.lua

--- __init__.lua
+++ __init__.lua
@@ -1,3 +1,16 @@
+/*  
+ __  __ _____ _   _       _____    
+|  \/  |  ___| | | | __ _|__  /___ 
+| |\/| | |_  | |_| |/ _` | / // _ \
+| |  | |  _| |  _  | (_| |/ /|  __/
+|_|  |_|_|   |_| |_|\__,_/____\___|
+                                                                  
+    Author:         MFHaZe
+    Creation Year:  2023
+    GitHub:         https://github.com/MFHaZe/H1-Mod-Third-Person                                                                
+*/
+
+	
 game:executecommand("Third Person Mod Loaded")
 
 function third()
